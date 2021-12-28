25f6170285f30e677744a145375c5a4cd625ad72
vivirose <vivien.marcolin@gmail.com>
2021-12-28 18:24:28 -0500

    corrected temp converter

--- src/GUIS/TempConverter/TempConverter.js
+++ src/GUIS/TempConverter/TempConverter.js
@@ -1,28 +1,40 @@
 import React, { useState } from "react";
 
 const TempConverter = () => {
-    const [celsius, setCelsius] = useState();
-    const [fahrenheit, setFahrenheit] = useState();
-    
-    function toCelsius(F) {
-        setCelsius(Math.round((F - 32) * (5/9)))
-    }
+  const [celsius, setCelsius] = useState(" ");
+  const [fahrenheit, setFahrenheit] = useState(" ");
 
-    function toFahrenheit(C) {
-        setFahrenheit(Math.round(C * (9/5) + 32))
-    }
+  function toCelsius(F) {
+    setFahrenheit(F);
+    F !== "" ? setCelsius(Math.round((F - 32) * (5 / 9))) : setCelsius(" ");
+  }
 
-    return(
-        <>
-        <h2>Temperature Converter</h2>
-        <form>
-            <input type="number" id="celsius" value={celsius} onChange={(e) => toFahrenheit(e.target.value)}></input>
-            <label htmlFor="celsius">Celsius =</label>
-            <input type="number" id="fahrenheit" value={fahrenheit} onChange={(e) => toCelsius(e.target.value)}></input>
-            <label htmlFor="fahrenheit">Fahrenheit</label>
-        </form>
-        </>
-    )
-}
+  function toFahrenheit(C) {
+    setCelsius(C);
+    C !== "" ? setFahrenheit(Math.round(C * (9 / 5) + 32)) : setFahrenheit(" ");
+  }
 
-export default TempConverter
+  return (
+    <>
+      <h2>Temperature Converter</h2>
+      <form>
+        <input
+          type="number"
+          id="celsius"
+          value={celsius}
+          onChange={(e) => toFahrenheit(e.target.value)}
+        ></input>
+        <label htmlFor="celsius">Celsius =</label>
+        <input
+          type="number"
+          id="fahrenheit"
+          value={fahrenheit}
+          onChange={(e) => toCelsius(e.target.value)}
+        ></input>
+        <label htmlFor="fahrenheit">Fahrenheit</label>
+      </form>
+    </>
+  );
+};
+
+export default TempConverter;
